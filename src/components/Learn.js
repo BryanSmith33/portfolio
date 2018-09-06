007bb63925efb45a505a3fbb123a28014705980f
Bryan Smith <bryan.gsmith33@gmail.com>
2018-09-06 13:13:15 -0600

    making some audit fixes

--- src/components/Learn.js
+++ src/components/Learn.js
@@ -36,22 +36,22 @@ class Learn extends Component {
       <LearnContainer>
         <TextContainer>
           <h1>Learn from/with me</h1>
-          <h3>I know, I know... <span role='image' aria-label='newspaper'>📰 </span><span role='image' aria-label='right arrow'> ➡️ </span><span role='image' aria-label='trash bin'>🗑</span></h3>
+          <h3>I know, I know... <span role='img' aria-label='newspaper'>📰 </span><span role='img' aria-label='right arrow'> ➡️ </span><span role='img' aria-label='trash bin'>🗑</span></h3>
           <p> But if you are wanting to learn web development without any fluff or BS tutorials, then I've got something for you.</p>
-          <p>I write up fresh web development articles (sometimes once a week if the children allow!!!) and you can get updated anytime I write something new by dropping your email in that there <span role='image' aria-label='point down'>👇</span> box and clicking "SEND ME THE GOODS!" </p>
+          <p>I write up fresh web development articles (sometimes once a week if the children allow!!!) and you can get updated anytime I write something new by dropping your email in that there <span role='img' aria-label='point down'>👇</span> box and clicking "SEND ME THE GOODS!" </p>
           <Form onSubmit={(e) => this.handleSubmit(e)}
             margin={`0 auto 1.5em`}>
             <Field>
-              <Input type="text" name="fullname" placeholder="Jeffrey Lebowski" required />
+              <Input type="text" name="fullname" placeholder="Jeffrey Lebowski" id="fullname" required />
               <Label for="fullname">Name</Label>
             </Field>
             <Field>
-              <Input type="email" name="email" placeholder="youare@awesome.com" required />
+              <Input type="email" name="email" placeholder="youare@awesome.com" id="email" required />
               <Label for="email">Email</Label>
             </Field>
             <FormButton>{this.state.subscribeMessage}</FormButton>
           </Form>
-          <p>P.S. I promise to never, ever spam you. So what do you say? Let's be internet friends <span role='image' aria-label='heart'>❤️</span><span role='image' aria-label='heart'>💚</span><span role='image' aria-label='heart'>💜</span></p>
+          <p>P.S. I promise to never, ever spam you. So what do you say? Let's be internet friends <span role='img' aria-label='heart'>❤️</span><span role='img' aria-label='heart'>💚</span><span role='img' aria-label='heart'>💜</span></p>
         </TextContainer>
       </LearnContainer>
     )
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 import Header from '../components/header'
 import './index.css'
+import favicon from '../images/favicon.png'
 
 const Layout = ({ children, data }) => (
   <div>
@@ -12,7 +13,12 @@ const Layout = ({ children, data }) => (
         { name: 'description', content: 'Bryan Smith is a web developer and designer from Salt Lake City, Utah' },
         { name: 'keywords', content: 'web developer, designer, freelance, design, salt lake city, utah' },
       ]}
-    />
+      link={[
+        { rel: 'shortcut icon', type: 'image/png', href: `${favicon}` }
+      ]}
+    >
+      <html lang="en" />
+    </Helmet>
     <Header siteTitle={data.site.siteMetadata.title} />
     <div>
       {children()}
