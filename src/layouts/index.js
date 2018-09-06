a417f93197fd2a3a6ed11083317d04395631601c
Bryan Smith <bryan.gsmith33@gmail.com>
2018-09-06 12:29:24 -0600

    who knows anymore

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,10 +1,8 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
-
 import Header from '../components/Header'
 import './index.css'
-
 
 const Layout = ({ children, data }) => (
   <div>
