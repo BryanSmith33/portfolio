39c44819b8a7f82bc8e36f250de95d92e7e8e438
Bryan Smith <bryan.gsmith33@gmail.com>
2018-12-16 00:13:16 +0100

    updated tattoo count

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,6 +1,15 @@
 import React from 'react'
 import styled from 'styled-components'
-import { FormButton, Container, Title, Form, Field, Input, Label, TextArea } from '../components/uiComponents'
+import {
+  FormButton,
+  Container,
+  Title,
+  Form,
+  Field,
+  Input,
+  Label,
+  TextArea,
+} from '../components/uiComponents'
 import Helmet from 'react-helmet'
 
 const FaqContainer = styled.div`
@@ -14,7 +23,7 @@ const FaqContainer = styled.div`
 `
 const AMA = styled.span`
   font-size: 3.3em;
-  line-height: .8em;
+  line-height: 0.8em;
   @media (max-width: 800px) {
     font-size: 2.3em;
   }
@@ -23,15 +32,24 @@ const AMA = styled.span`
 const Contact = () => (
   <Container>
     <Helmet
-      title='Contact | Bryan Smith Salt Lake City Web Developer & Designer'
+      title="Contact | Bryan Smith Salt Lake City Web Developer & Designer"
       meta={[
         { name: 'description', content: 'Contact Bryan Smith' },
-        { name: 'keywords', content: 'web developer, designer, freelance, design, salt lake city, utah' },
+        {
+          name: 'keywords',
+          content:
+            'web developer, designer, freelance, design, salt lake city, utah',
+        },
       ]}
     />
     <FaqContainer>
-      <Title>Go ahead, <br /> <AMA>AMA</AMA></Title>
-      <p>That's Ask Me Anything incase you didn't know. Here are a few quick facts about me just in case it answers any questions you might have:</p>
+      <Title>
+        Go ahead, <br /> <AMA>AMA</AMA>
+      </Title>
+      <p>
+        That's Ask Me Anything incase you didn't know. Here are a few quick
+        facts about me just in case it answers any questions you might have:
+      </p>
       <ul>
         <li>Broken Bones: 0 (1 chipped though)</li>
         <li>Favorit Color: red</li>
@@ -42,20 +60,40 @@ const Contact = () => (
         <li>Height: 6'1"</li>
         <li>Greatest Fear: Being chased up the stairs in the dark</li>
         <li>Favorite Vacation: Hawaii</li>
-        <li>Tattoos: 4</li>
+        <li>Tattoos: 5</li>
       </ul>
     </FaqContainer>
-    <Form margin={`initial`} data-netlify="true" data-netlify-honeypot="bot-field" name='ama'>
+    <Form
+      margin={`initial`}
+      data-netlify="true"
+      data-netlify-honeypot="bot-field"
+      name="ama"
+    >
       <Field>
-        <Input type="text" name="fullname" placeholder="Jeffrey Lebowski" id='fullname' required />
+        <Input
+          type="text"
+          name="fullname"
+          placeholder="Jeffrey Lebowski"
+          id="fullname"
+          required
+        />
         <Label for="fullname">Name</Label>
       </Field>
       <Field>
-        <Input type="email" name="email" placeholder="youare@awesome.com" id='email' required />
+        <Input
+          type="email"
+          name="email"
+          placeholder="youare@awesome.com"
+          id="email"
+          required
+        />
         <Label for="email">Email</Label>
       </Field>
       <Field>
-        <TextArea placeholder='Hopefully something flattering...'></TextArea>
+        <TextArea
+          name="message"
+          placeholder="Hopefully something flattering..."
+        />
       </Field>
       <FormButton>Questions Away</FormButton>
     </Form>
