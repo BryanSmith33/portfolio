1db02365665eaae107f0c1606216372daa53f6ab
Bryan Smith <bryan.gsmith33@gmail.com>
2019-04-25 11:30:31 -0600

    fixed path to Header

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,12 +1,4 @@
-/**
- * Layout component that queries for data
- * with Gatsby's StaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/static-query/
- */
-
 import React from "react"
-import PropTypes from "prop-types"
 import { StaticQuery, graphql } from "gatsby"
 
 import Header from "./Header"
@@ -34,8 +26,5 @@ const Layout = ({ children }) => (
   />
 )
 
-Layout.propTypes = {
-  children: PropTypes.node.isRequired,
-}
 
 export default Layout
